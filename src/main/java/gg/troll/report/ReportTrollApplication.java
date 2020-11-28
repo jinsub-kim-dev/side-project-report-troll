6feec4d4820b6869c91874a4b43aa82084a35d45
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2020-11-28 15:34:50 +0900

    initial commit

---
+++ src/main/java/gg/troll/report/ReportTrollApplication.java
@@ -0,0 +1,13 @@
+package gg.troll.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportTrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportTrollApplication.class, args);
+	}
+
+}
