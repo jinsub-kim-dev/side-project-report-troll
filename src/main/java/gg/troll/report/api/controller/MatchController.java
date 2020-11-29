2f62419f85ccbf56f60c4ff5003dfd53314c37c1
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2020-11-29 16:02:12 +0900

    라이엇 API 메소드와 운영용 API 메소드를 분리

--- src/main/java/gg/troll/report/api/controller/MatchController.java
+++ src/main/java/gg/troll/report/api/controller/MatchController.java
@@ -1,8 +1,7 @@
 package gg.troll.report.api.controller;
 
-import gg.troll.report.api.match.model.MatchDto;
-import gg.troll.report.api.match.model.MatchlistDto;
 import gg.troll.report.api.match.service.MatchService;
+import gg.troll.report.api.summoner.model.SummonerDTO;
 import gg.troll.report.base.model.CodeResponse;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -12,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -21,26 +19,4 @@ public class MatchController {
 
     @Autowired
     MatchService matchService;
-
-    @GetMapping("/list/encryptedAccountId")
-    @ResponseBody
-    @ApiOperation(value = "(Riot 제공 API) 계정 아이디로 매치 리스트 조회.", response = MatchlistDto.class)
-    @ApiImplicitParams({@ApiImplicitParam(name = "Riot-API-Key",  required = true, dataType = "string", paramType = "header")})
-    public CodeResponse getMatchListByEncryptedAccountId(
-            @ApiParam(hidden = true) String riotApiKey,
-            @RequestParam String encryptedAccountId,
-            @RequestParam(required = false, defaultValue = "0") int beginIndex,
-            @RequestParam(required = false, defaultValue = "0") int endIndex) throws Exception {
-        MatchlistDto matchlistDto = matchService.getMatchListByEncryptedAccountId(riotApiKey, encryptedAccountId, beginIndex, endIndex);
-        return CodeResponse.successResult(matchlistDto);
-    }
-
-    @GetMapping("/id")
-    @ResponseBody
-    @ApiOperation(value = "(Riot 제공 API) 매치 아이디로 세부 매치 정보 조회.", response = MatchDto.class)
-    @ApiImplicitParams({@ApiImplicitParam(name = "Riot-API-Key",  required = true, dataType = "string", paramType = "header")})
-    public CodeResponse getMatchById(@ApiParam(hidden = true) String riotApiKey, @RequestParam long matchId) throws Exception {
-        MatchDto matchDto = matchService.getMatchById(riotApiKey, matchId);
-        return CodeResponse.successResult(matchDto);
-    }
 }
--- src/main/java/gg/troll/report/api/controller/SummonerController.java
+++ src/main/java/gg/troll/report/api/controller/SummonerController.java
@@ -1,18 +1,9 @@
 package gg.troll.report.api.controller;
 
-import gg.troll.report.api.summoner.model.SummonerDTO;
 import gg.troll.report.api.summoner.service.SummonerService;
-import gg.troll.report.base.model.CodeResponse;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api/v1/summoner")
@@ -21,12 +12,5 @@ public class SummonerController {
     @Autowired
     SummonerService summonerService;
 
-    @GetMapping("/name")
-    @ResponseBody
-    @ApiOperation(value = "(Riot 제공 API) 닉네임으로 계정 정보 조회.", response = SummonerDTO.class)
-    @ApiImplicitParams({@ApiImplicitParam(name = "Riot-API-Key",  required = true, dataType = "string", paramType = "header")})
-    public CodeResponse getSummonerByName(@ApiParam(hidden = true) String riotApiKey, @RequestParam String summonerName) throws Exception {
-        SummonerDTO summonerDTO = summonerService.getSummonerByName(riotApiKey, summonerName);
-        return CodeResponse.successResult(summonerDTO);
-    }
+
 }
