51ffa7035d6ce1cb53dd1488c409e2e8e489324a
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2020-11-29 20:59:15 +0900

    Assessment Entity 정의 및 Time Auditing 설정

---
+++ src/main/java/gg/troll/report/api/assessment/model/entity/Assessment.java
@@ -0,0 +1,38 @@
+package gg.troll.report.api.assessment.model.entity;
+
+import gg.troll.report.api.assessment.enums.AssessmentType;
+import gg.troll.report.base.model.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_assessment")
+public class Assessment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long assessmentId;
+
+    @Column
+    private long gameId;
+
+    @Column
+    private String accountId;
+
+    @Column
+    @Enumerated(EnumType.ORDINAL)
+    AssessmentType assessmentType;
+
+    @Column
+    private String comment;
+
+    @Column
+    private String hashedPassword;
+}
