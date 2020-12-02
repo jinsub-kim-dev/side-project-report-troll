988bf2d86f025551ff207191222a2ec6b3ccf937
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2020-12-02 17:43:56 +0900

    Summoner API를 Swagger에서 호출 시 Riot Api Key를 헤더로 받을 수 있도록 변경

--- src/main/java/gg/troll/report/api/controller/SummonerController.java
+++ src/main/java/gg/troll/report/api/controller/SummonerController.java
@@ -3,6 +3,8 @@ package gg.troll.report.api.controller;
 import gg.troll.report.api.summoner.model.LeagueSummonerDTO;
 import gg.troll.report.api.summoner.service.SummonerService;
 import gg.troll.report.base.model.CodeResponse;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,6 +22,7 @@ public class SummonerController {
 
     @GetMapping("/name")
     @ResponseBody
+    @ApiImplicitParams({@ApiImplicitParam(name = "Riot-API-Key",  required = true, dataType = "string", paramType = "header")})
     public CodeResponse getLeagueSummonerByName(@ApiParam(hidden = true) String riotApiKey,  @RequestParam String summonerName) throws Exception {
         LeagueSummonerDTO leagueSummonerDTO = summonerService.getLeagueSummonerByName(riotApiKey, summonerName);
         return CodeResponse.successResult(leagueSummonerDTO);
