aaa244a060ba744d48d07644d69ad112001c436c
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2020-12-03 11:23:05 +0900

    Assessment API에 Swagger ApiOperation 문서 추가

--- src/main/java/gg/troll/report/api/controller/AssessmentController.java
+++ src/main/java/gg/troll/report/api/controller/AssessmentController.java
@@ -8,6 +8,7 @@ import gg.troll.report.api.assessment.model.request.AssessmentCreateRequest;
 import gg.troll.report.api.assessment.model.request.AssessmentDeleteRequest;
 import gg.troll.report.api.assessment.service.AssessmentService;
 import gg.troll.report.base.model.CodeResponse;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +25,7 @@ public class AssessmentController {
     @CrossOrigin("*")
     @PostMapping("")
     @ResponseBody
+    @ApiOperation(value = "평가 작성", response = AssessmentDto.class)
     public CodeResponse createAssessment(@RequestBody AssessmentCreateRequest request) throws NoSuchAlgorithmException {
         Assessment savedAssessment = assessmentService.createAssessment(request.getGameId(), request.getAccountId(),
                 request.getAssessmentType(), request.getComment(), request.getPassword());
@@ -33,6 +35,7 @@ public class AssessmentController {
     @CrossOrigin("*")
     @GetMapping("/id")
     @ResponseBody
+    @ApiOperation(value = "id로 평가 단일 조회", response = AssessmentDto.class)
     public CodeResponse getAssessmentById(@RequestParam long assessmentId) {
         AssessmentDto assessmentDto = assessmentService.getAssessmentDtoById(assessmentId);
         return CodeResponse.successResult(assessmentDto);
@@ -41,6 +44,7 @@ public class AssessmentController {
     @CrossOrigin("*")
     @GetMapping("/list/account/game")
     @ResponseBody
+    @ApiOperation(value = "gameId와 accountId에 해당하는 평가 리스트 조회", response = AssessmentListDto.class)
     public CodeResponse getAssessmentListByGameAndAccountId(
             @RequestParam long gameId,
             @RequestParam String accountId,
@@ -53,6 +57,7 @@ public class AssessmentController {
     @CrossOrigin("*")
     @PutMapping("/comment")
     @ResponseBody
+    @ApiOperation(value = "평가 내용 수정", response = CodeResponse.class)
     public CodeResponse updateAssessmentComment(@RequestBody AssessmentCommentRequest request) throws NoSuchAlgorithmException {
         assessmentService.updateAssessmentComment(request.getAssessmentId(), request.getComment(), request.getPassword());
         return CodeResponse.success();
@@ -61,6 +66,7 @@ public class AssessmentController {
     @CrossOrigin("*")
     @DeleteMapping("/id")
     @ResponseBody
+    @ApiOperation(value = "평가 삭제", response = CodeResponse.class)
     public CodeResponse deleteAssessment(@RequestBody AssessmentDeleteRequest request) throws NoSuchAlgorithmException {
         assessmentService.deleteAssessment(request.getAssessmentId(), request.getPassword());
         return CodeResponse.success();
