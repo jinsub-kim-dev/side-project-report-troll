e62236ff5452035bf8be24284728243dadbdd082
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2020-12-11 00:22:11 +0900

    일반, 우르프, 봇전 queuetype 추가

--- src/main/java/gg/troll/report/api/match/enums/QueueType.java
+++ src/main/java/gg/troll/report/api/match/enums/QueueType.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 @Getter
 public enum QueueType {
     RIFT_SOLO_RANK(420,"RANKED_SOLO_5x5", "솔로랭크"),
+    RIFT_BLIND_PICK(430, "", "일반"),
     RIFT_FLEX_RANK(440, "RANKED_FLEX_SR", "자유 5:5 랭크"),
     ARAM(450, "", "무작위 총력전"),
+    AI_BOT(850, "", "Bot"),
+    URF(900, "", "우르프"),
     UNKNOWN(0, "", "지원하지 않는 타입");
 
     int queueId;
