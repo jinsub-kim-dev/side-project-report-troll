497c95602963cb94816b9edb8ce08b997dc2d199
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2020-12-15 01:55:51 +0900

    초기 데이터를 한번에 받아오도록 하는 Home API 추가

---
+++ src/main/java/gg/troll/report/api/home/service/HomeService.java
@@ -0,0 +1,40 @@
+package gg.troll.report.api.home.service;
+
+import gg.troll.report.api.assessment.model.dto.AssessmentListDto;
+import gg.troll.report.api.assessment.model.dto.SummonerAssessmentMetaDto;
+import gg.troll.report.api.assessment.service.AssessmentService;
+import gg.troll.report.api.home.model.AssessmentsRequest;
+import gg.troll.report.api.home.model.MatchesRequest;
+import gg.troll.report.api.home.model.dto.HomeResponseDto;
+import gg.troll.report.api.match.model.ReducedMatchlistDto;
+import gg.troll.report.api.match.service.MatchService;
+import gg.troll.report.api.summoner.model.LeagueSummonerDTO;
+import gg.troll.report.api.summoner.model.SummonerDTO;
+import gg.troll.report.api.summoner.service.SummonerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+    @Autowired
+    SummonerService summonerService;
+    @Autowired
+    MatchService matchService;
+    @Autowired
+    AssessmentService assessmentService;
+
+    public HomeResponseDto getHomeResponseDto(String riotApiKey, String summonerName, AssessmentsRequest assessmentsRequest, MatchesRequest matchesRequest) throws Exception {
+        SummonerDTO summonerDTO = summonerService.getSummonerByName(riotApiKey, summonerName);
+        SummonerAssessmentMetaDto summonerAssessmentMetaDto = assessmentService.getSummonerAssessmentMetaDto(summonerDTO.getAccountId());
+        AssessmentListDto assessmentListDto = assessmentService.getAssessmentListDtoByAccountId(summonerDTO.getAccountId(), assessmentsRequest.getFromAssessmentId(), assessmentsRequest.getSize());
+        LeagueSummonerDTO leagueSummonerDTO = summonerService.getLeagueSummonerBySummonerDto(riotApiKey, summonerDTO);
+        ReducedMatchlistDto reducedMatchlistDto = matchService.getReducedMatchListBySummonerDto(riotApiKey, summonerDTO, matchesRequest.getBeginIndex(), matchesRequest.getEndIndex());
+
+        return HomeResponseDto.builder()
+                .summonerAssessmentMeta(summonerAssessmentMetaDto)
+                .assessments(assessmentListDto)
+                .leagueSummoner(leagueSummonerDTO)
+                .matches(reducedMatchlistDto)
+                .build();
+    }
+}
